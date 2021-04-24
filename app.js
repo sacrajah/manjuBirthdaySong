b27d81f839e55b959287deebc71ca541fc041c5e
Ranger <30779082+sacrajah@users.noreply.github.com>
2021-04-25 00:09:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var x = document.getElementById("HBD"); 
+  
+function playAudio() { 
+  x.play(); 
+} 
+setInterval(playAudio, 3000);
+function pauseAudio() { 
+  x.pause(); 
+} 
+
+const img = document.getElementById('img')
+  const btn = document.querySelector('button')
+  const display =document.querySelector('#display')
+  btn.addEventListener('click', ()=>{
+   display.innerHTML = '<h1 style= "font-size: 50px ;color:red"> Happy Birthday,  Manju ! ,  Mama !,Atthe <br><h2></h2> 25th April <br><h4> Me, Rich ,Caro, Jemima </h4>'
+   img.classList.add('swing')
+
+  })
+  btn.addEventListener('click' , playAudio)
